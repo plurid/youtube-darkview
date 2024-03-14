a94554806e086524204636ea30c44f30aa371243
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-03-14 21:35:09 +0200

    dev: contentscript | concept

--- packages/youtube-darkview/source/contentscript/index.ts
+++ packages/youtube-darkview/source/contentscript/index.ts
@@ -20,27 +20,29 @@ const toggleDarkview = async () => {
     }
 
     if (!toggled) {
+        const videoDimensions = video.getBoundingClientRect();
+
         const canvas = document.createElement('canvas');
         canvas.id = CANVAS_ID;
         canvas.style.position = 'absolute';
-        canvas.style.top = '50px';
+        canvas.style.top = '56px';
         canvas.style.left = '0';
         canvas.style.right = '0';
         canvas.style.width = '100%';
-        canvas.style.height = '500px';
+        canvas.style.height = videoDimensions.height + 'px';
+        canvas.style.zIndex = '58';
         canvas.style.pointerEvents = 'none';
 
         document.body.appendChild(canvas);
 
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d', {
+            willReadFrequently: true,
+        });
 
         setTimeout(() => {
-            const videoComputedStyle = window.getComputedStyle(video);
+            const videoContentWidth = videoDimensions.width;
+            const videoContentHeight = videoDimensions.height;
 
-            const videoContentWidth = parseInt(videoComputedStyle.width, 10);
-            const videoContentHeight = parseInt(videoComputedStyle.height, 10);
-
-            // var aspectRatio = video.videoWidth / video.videoHeight;
             const aspectRatio = videoContentWidth / videoContentHeight;
             const canvasWidth = videoContentWidth;
             const canvasHeight = canvasWidth / aspectRatio;
@@ -50,22 +52,21 @@ const toggleDarkview = async () => {
             // 30 frames per second (1000 ms / 30 frames ≈ 33 ms per frame)
             const FPS_TIMEOUT = 33;
 
-            video.addEventListener('play', function() {
-                interval = setInterval(function() {
-                    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            interval = setInterval(function() {
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-                    var imageData = ctx.getImageData(0, 0, 800, 500);
-                    var data = imageData.data;
-                    for (var i = 0; i < data.length; i += 4) {
-                        data[i] = 255 - data[i]; // Red
-                        data[i + 1] = 255 - data[i + 1]; // Green
-                        data[i + 2] = 255 - data[i + 2]; // Blue
-                    }
-                    ctx.putImageData(imageData, 0, 0);
+                // const imageData = ctx.getImageData(0, 0, 600, 500);
+                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                const data = imageData.data;
+                for (let i = 0; i < data.length; i += 4) {
+                    data[i] = 255 - data[i]; // Red
+                    data[i + 1] = 255 - data[i + 1]; // Green
+                    data[i + 2] = 255 - data[i + 2]; // Blue
+                }
+                ctx.putImageData(imageData, 0, 0);
 
-                    ctx.clearRect(0, canvas.height - 60, canvas.width, 50);
-                }, FPS_TIMEOUT);
-            }, false);
+                // ctx.clearRect(0, canvas.height - 60, canvas.width, 50);
+            }, FPS_TIMEOUT);
         }, 200);
     }
 

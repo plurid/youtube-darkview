043cc37fd311fc94169a09144214e6bc68a3666f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-03-15 09:38:18 +0200

    dev: contentscript

--- packages/youtube-darkview/source/contentscript/index.ts
+++ packages/youtube-darkview/source/contentscript/index.ts
@@ -10,6 +10,137 @@ const threshold = 0.6;
 const limit = 255;
 
 
+
+const getComputes = (
+    canvas: HTMLCanvasElement,
+    video: HTMLVideoElement,
+) => {
+    const ctx = canvas.getContext('2d', {
+        willReadFrequently: true,
+    });
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    const whiteThreshold = 255 * threshold;
+
+    return {
+        ctx,
+        imageData,
+        data,
+        whiteThreshold,
+    };
+}
+
+const computeDarkViewRaw = (
+    canvas: HTMLCanvasElement,
+    video: HTMLVideoElement,
+) => {
+    const {
+        ctx,
+        imageData,
+        data,
+        whiteThreshold,
+    } = getComputes(canvas, video);
+
+    // const start = Date.now();
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const blocksWide = Math.ceil(canvas.width / blockSize);
+    const blocksHigh = Math.ceil(canvas.height / blockSize);
+    const whiteBlocks = [];
+    const staticBlocks = [];
+
+    for (let y = 0; y < blocksHigh; y++) {
+        for (let x = 0; x < blocksWide; x++) {
+            let whitePixelCount = 0;
+            const startX = x * blockSize;
+            const endX = Math.min(startX + blockSize, canvas.width);
+            const startY = y * blockSize;
+            const endY = Math.min(startY + blockSize, canvas.height);
+
+            for (let blockY = startY; blockY < endY; blockY++) {
+                for (let blockX = startX; blockX < endX; blockX++) {
+                    const dataIndex = (blockY * canvas.width + blockX) * 4;
+                    const r = data[dataIndex];
+                    const g = data[dataIndex + 1];
+                    const b = data[dataIndex + 2];
+                    // Check if pixel is white
+                    if (r >= whiteThreshold && g >= whiteThreshold && b >= whiteThreshold) {
+                        whitePixelCount++;
+                    }
+                }
+            }
+
+            const whitePixelPercentage = whitePixelCount / (blockSize * blockSize);
+
+            if (whitePixelPercentage >= threshold) {
+                whiteBlocks.push({ x, y, whitePixelPercentage });
+            } else {
+                staticBlocks.push({ x, y, whitePixelPercentage });
+            }
+        }
+    }
+
+    for (const block of whiteBlocks) {
+        const startX = block.x * blockSize;
+        const endX = Math.min(startX + blockSize, canvas.width);
+        const startY = block.y * blockSize;
+        const endY = Math.min(startY + blockSize, canvas.height);
+
+        for (let y = startY; y < endY; y++) {
+            for (let x = startX; x < endX; x++) {
+                const dataIndex = (y * canvas.width + x) * 4;
+                data[dataIndex] = 255 - data[dataIndex]; // Invert red channel
+                data[dataIndex + 1] = 255 - data[dataIndex + 1]; // Invert green channel
+                data[dataIndex + 2] = 255 - data[dataIndex + 2]; // Invert blue channel
+                // Alpha channel remains unchanged
+            }
+        }
+    }
+
+    // console.log({
+    //     whiteBlocks, staticBlocks,
+    // });
+    // const endCompute = Date.now();
+    // console.log('compute', endCompute - start);
+
+    ctx.putImageData(imageData, 0, 0);
+
+    // const endPaint = Date.now();
+    // console.log('paint', endPaint - endCompute);
+    // console.log('---');
+}
+
+const computeDarkViewQuadTree = (
+    canvas: HTMLCanvasElement,
+    video: HTMLVideoElement,
+) => {
+    const {
+        ctx,
+        imageData,
+        data,
+        whiteThreshold,
+    } = getComputes(canvas, video);
+
+    // const quadtree = new QuadTreeNode(0, 0, canvas.width);
+    // quadtree.update(imageData, whiteThreshold);
+    // quadtree.invertColors(imageData, 3);
+    // ctx.putImageData(imageData, 0, 0);
+}
+
+const computeDarkView = (
+    canvas: HTMLCanvasElement,
+    video: HTMLVideoElement,
+) => {
+    computeDarkViewRaw(canvas, video);
+    // computeDarkViewQuadTree(canvas, video);
+}
+
+
 const toggleDarkview = async () => {
     const canvas = document.getElementById(CANVAS_ID);
     if (canvas) {
@@ -42,10 +173,6 @@ const toggleDarkview = async () => {
 
         document.body.appendChild(canvas);
 
-        const ctx = canvas.getContext('2d', {
-            willReadFrequently: true,
-        });
-
         setTimeout(() => {
             const videoContentWidth = videoDimensions.width;
             const videoContentHeight = videoDimensions.height;
@@ -60,63 +187,7 @@ const toggleDarkview = async () => {
             const FPS_TIMEOUT = 33;
 
             interval = setInterval(function() {
-                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                const data = imageData.data;
-
-                const blocksWide = Math.ceil(canvas.width / blockSize);
-                const blocksHigh = Math.ceil(canvas.height / blockSize);
-                const whiteThreshold = 255 * threshold;
-                const whiteBlocks = [];
-
-                for (let y = 0; y < blocksHigh; y++) {
-                    for (let x = 0; x < blocksWide; x++) {
-                        let whitePixelCount = 0;
-                        const startX = x * blockSize;
-                        const endX = Math.min(startX + blockSize, canvas.width);
-                        const startY = y * blockSize;
-                        const endY = Math.min(startY + blockSize, canvas.height);
-
-                        for (let blockY = startY; blockY < endY; blockY++) {
-                            for (let blockX = startX; blockX < endX; blockX++) {
-                                const dataIndex = (blockY * canvas.width + blockX) * 4;
-                                const r = data[dataIndex];
-                                const g = data[dataIndex + 1];
-                                const b = data[dataIndex + 2];
-                                // Check if pixel is white
-                                if (r >= whiteThreshold && g >= whiteThreshold && b >= whiteThreshold) {
-                                    whitePixelCount++;
-                                }
-                            }
-                        }
-
-                        const whitePixelPercentage = whitePixelCount / (blockSize * blockSize);
-
-                        if (whitePixelPercentage >= threshold) {
-                            whiteBlocks.push({ x, y, whitePixelPercentage });
-                        }
-                    }
-                }
-
-                for (const block of whiteBlocks) {
-                    const startX = block.x * blockSize;
-                    const endX = Math.min(startX + blockSize, canvas.width);
-                    const startY = block.y * blockSize;
-                    const endY = Math.min(startY + blockSize, canvas.height);
-
-                    for (let blockY = startY; blockY < endY; blockY++) {
-                        for (let blockX = startX; blockX < endX; blockX++) {
-                            const dataIndex = (blockY * canvas.width + blockX) * 4;
-                            // Invert colors
-                            data[dataIndex] = 255 - data[dataIndex]; // Red
-                            data[dataIndex + 1] = 255 - data[dataIndex + 1]; // Green
-                            data[dataIndex + 2] = 255 - data[dataIndex + 2]; // Blue
-                        }
-                    }
-                }
-
-                ctx.putImageData(imageData, 0, 0);
+                computeDarkView(canvas, video);
             }, FPS_TIMEOUT);
         }, 200);
     }
